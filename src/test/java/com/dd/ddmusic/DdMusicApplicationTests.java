e4b032bfb2b65260164576bd8b4d8fac75257203
DD <892791207@qq.com>
2022-01-08 22:21:49 +0800

    first commit

---
+++ src/test/java/com/dd/ddmusic/DdMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dd.ddmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
